6bceea20275a6fd87bf6ffc8569ee3857f7519c0
PaulCrtr <paul.th.cartier@gmail.com>
2020-04-20 11:36:45 +0200

    challenge done

---
+++ src/Counter.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Le compteur est à : {count} </p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <input
+        onChange={(e) => setCount(parseInt(e.target.value))}
+        placeholder="valeur de départ"
+      ></input>
+    </div>
+  );
+}
+
+export default Counter;
